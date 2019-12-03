6f205cf92a09cec8ff08bbbb6cbe6b5c76da468d
Monica Ocampo <moocampo@csumb.edu>
2019-12-03 20:41:03 +0000

    updates

--- public/labs/lab9/router.js
+++ public/labs/lab9/router.js
@@ -1,21 +1,21 @@
-// const express = require('express');
-// const router = express.Router();
-// const mysql = require('mysql');
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
 
-// /* GET users listening. (/mysql) */
-// router.get('/', function(req, res, next) {
+/* GET users listening. (/mysql) */
+router.get('/', function(req, res, next) {
     
-//     // GET A QUERY STRING VALUE FOR FILTER
-//     const nameFilter = req.query.name; 
+    // GET A QUERY STRING VALUE FOR FILTER
+    const nameFilter = req.query.name; 
 
-//     const connection = mysql.createConnection({
-//         host: 'ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//         user: 'pqai7eyjjnf44uh5',
-//         password: 'tg0cclip6o8u6n6b',
-//         database: 'ddwnbsxcdd0glq33'
-//     });
+    const connection = mysql.createConnection({
+        host: 'ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'pqai7eyjjnf44uh5',
+        password: 'tg0cclip6o8u6n6b',
+        database: 'ddwnbsxcdd0glq33'
+    });
 
-//     connection.connect();
+    connection.connect();
 
 //     connection.query(`
 //     SELECT q.*, CONCAT(a.firstName, ' ', a.lastName) AS 'fullName', a.sex AS 'gender'
@@ -34,54 +34,34 @@
 // });
 
 // module.exports = router;
-const express = require('express');
-const router = express.Router();
-const mysql = require('mysql');
+// const express = require('express');
+// const router = express.Router();
+// const mysql = require('mysql');
 
-/* GET users listening. (/mysql) */
-router.get('/quotes', function(req, res, next) {
+// /* GET users listening. (/mysql) */
+// router.get('/quotes', function(req, res, next) {
     
-    // GET A QUERY STRING VALUE FOR FILTER
-    const nameFilter = req.query.name; 
+//     // GET A QUERY STRING VALUE FOR FILTER
+//     const nameFilter = req.query.name; 
 
-    const connection = mysql.createConnection({
-        host: 'ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'pqai7eyjjnf44uh5',
-        password: 'tg0cclip6o8u6n6b',
-        database: 'ddwnbsxcdd0glq33'
-    });
+//     const connection = mysql.createConnection({
+//         host: 'ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//         user: 'pqai7eyjjnf44uh5',
+//         password: 'tg0cclip6o8u6n6b',
+//         database: 'ddwnbsxcdd0glq33'
+//     });
 
-    connection.connect();
+//     connection.connect();
     
-    connection.query(
-    `SELECT category 
-    FROM l9_quotes
-    GROUP BY category`, function(error, results, fields) {
-        if (error) throw error; 
+//     connection.query(
+//     `SELECT category 
+//     FROM l9_quotes
+//     GROUP BY category`, function(error, results, fields) {
+//         if (error) throw error; 
         
-        res.render('../public/labs/lab9_quotes/view', {
-            title: 'Lab9: Quotes Database', 
-            category: results
-        });
-    });
-
-    connection.end();
-});
-
-module.exports = router;
-
-//     connection.query(`
-//     SELECT DISTINCT CONCAT(a.firstName, ' ', a.lastName) AS 'authorName'
-//     FROM l9_favorite f INNER JOIN
-//     l9_quotes q ON f.quoteId = q.quoteId INNER JOIN
-//     l9_author a ON q.authorId = a.authorId  
-//     `, function(error, results, fields) {
-//         if (error) throw error;
-
 //         res.render('../public/labs/lab9/view', {
-//             title: 'Lab 9', 
-//             quotes: results
-            
+//             title: 'Lab9: Quotes Database', 
+//             category: results
 //         });
 //     });
 
@@ -89,3 +69,23 @@ module.exports = router;
 // });
 
 // module.exports = router;
+
+    connection.query(`
+    SELECT DISTINCT CONCAT(a.firstName, ' ', a.lastName) AS 'authorName'
+    FROM l9_favorite f INNER JOIN
+    l9_quotes q ON f.quoteId = q.quoteId INNER JOIN
+    l9_author a ON q.authorId = a.authorId  
+    `, function(error, results, fields) {
+        if (error) throw error;
+
+        res.render('../public/labs/lab9/view', {
+            title: 'Lab 9', 
+            quotes: results
+            
+        });
+    });
+
+    connection.end();
+});
+
+module.exports = router;
